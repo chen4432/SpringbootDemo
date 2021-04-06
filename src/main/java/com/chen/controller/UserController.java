fcbf4b76d3b71ef3fe8e99bb103096f3633315cc
chen4432 <1576364432@qq.com>
2021-04-06 23:05:37 +0800

    第一次提交HelloController

---
+++ src/main/java/com/chen/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.chen.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作者: chen
+ * 时间: 2021/4/6 23:02
+ */
+@RestController
+public class UserController {
+    public String hello(){
+        return "hello";
+    }
+}
